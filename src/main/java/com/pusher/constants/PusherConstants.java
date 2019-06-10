c421597c8c2672d78e76a321145ba5b6396b0737
ubaid showkat <ubaidshowkat91@gmail.com>
2019-06-10 20:41:30 +1000

    First commint

---
+++ src/main/java/com/pusher/constants/PusherConstants.java
@@ -0,0 +1,18 @@
+package com.pusher.constants;
+
+public interface PusherConstants {
+	
+	/** Pusher App ID */
+	String PUSHER_APP_ID = "793972";
+	
+	/** Pusher App Key */
+	String PUSHER_APP_KEY = "5c8c8b038130313521da";
+	
+	/** Pusher App Secret */
+	String PUSHER_APP_SECRET = "be9b9fd0a93339d843e9";
+
+	String PUSHER_CLUSTER_KEY = "ap4";
+
+	/** Pusher channel name where the events will be published */
+	String CHANNEL_NAME = "shopping-cart";
+}
